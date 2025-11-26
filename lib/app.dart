624572fc49c18670b043c39c52dbe44070bf3250
amana-nminustwo <amana.nminustwo@gmail.com>
2025-11-26 11:49:21 +0530

    implemented login with google page on 26/11/25 at 11.49 am

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:learn_ease/presentation/screens/login/login_auth.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'LearnEase',
+      builder: EasyLoading.init(),
+      home:LoginAuth(),
+    );
+  }
+}
